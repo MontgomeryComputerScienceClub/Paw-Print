85e4f514befb4e8f9aec8fd0deb25f76a7c349ac
Eddie <eddietang2314@gmail.com>
2024-10-25 15:55:09 -0400

    Feature: article preview + story model

---
+++ lib/src/models/story.dart
@@ -0,0 +1,31 @@
+class Story {
+  // double unweighted;
+  // double weighted;
+
+  // Gpa({required this.unweighted, required this.weighted});
+
+  // Map<String, dynamic> toJson() {
+  //   return {
+  //     'unweighted': unweighted,
+  //     'weighted': weighted,
+  //   };
+  // }
+
+  // factory Gpa.fromJson(Map<String, dynamic> json) {
+  //   return Gpa(
+  //     unweighted: json['unweighted'].toDouble(),
+  //     weighted: json['weighted'].toDouble(),
+  //   );
+  // }
+
+  // @override
+  // bool operator ==(Object other) =>
+  //     identical(this, other) ||
+  //     other is Gpa &&
+  //         runtimeType == other.runtimeType &&
+  //         unweighted == other.unweighted &&
+  //         weighted == other.weighted;
+
+  // @override
+  // int get hashCode => unweighted.hashCode ^ weighted.hashCode;
+}
