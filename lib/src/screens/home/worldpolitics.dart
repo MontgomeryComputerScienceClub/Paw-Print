e32fa95cfffc622de4ecc7c5f9fee2a809caa4b6
Eddie <eddietang2314@gmail.com>
2024-12-12 12:36:58 -0500

    Feature: World politics set up done

---
+++ lib/src/screens/home/worldpolitics.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//use https://pub.dev/packages/flutter_earth_globe
+// need to modify story or story preview to include location ig idk
+class WorldPolitics extends StatelessWidget {
+  const WorldPolitics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
